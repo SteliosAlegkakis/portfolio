666976992e7c4eded36ad270cf13e8c87ca2ba09
SteliosAlegkakis <stelios.alegak@gmail.com>
2023-03-12 16:27:18 +0200

    added menu for mobile

---
+++ app.js
@@ -0,0 +1,7 @@
+function showMenu(){
+    document.getElementById("mobile_menu").style.display = 'flex';
+}
+
+function hideMenu(){
+    document.getElementById("mobile_menu").style.display = 'none';
+}
